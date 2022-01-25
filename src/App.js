e29c7fc3b658344be43b3ace259c300847421953
Sirrius <48409029+Sirrius-001@users.noreply.github.com>
2022-01-25 22:13:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -199,7 +199,7 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/rickgarage.png" : null}
       >
         <a href={CONFIG.MARKETPLACE_LINK}>
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
